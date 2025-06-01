5db05b336ef9f230f029ed6d19123064ca5fa215
Biyse <mrs.busecici@gmail.com>
2025-06-01 13:38:57 +0300

    dajd

---
+++ Assets/scripts/SoundManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField]private AudioSource audiosource;
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    public void PlaySound(AudioClip _sound) {
+        audiosource.PlayOneShot(_sound);
+    }
+}
